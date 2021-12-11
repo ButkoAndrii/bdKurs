5def6ea2170f353b774396fbd00db3e6073c6a94
ButkoAndrii <[flowebwoo@gmail.com]>
2021-12-11 13:15:32 +0200

    Changes

--- db/db/databaseProject/main/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Person, QueueConscripts
-
-admin.site.register(Person)
-admin.site.register(QueueConscripts)
--- db/db/databaseProject/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('current_query', views.current_query),
-]
